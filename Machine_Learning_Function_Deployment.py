76974b21e6fe77016f44e6b67f88a41d3580031d
Karan Mehta <56735251+karmeh18@users.noreply.github.com>
2023-02-23 19:46:43 +0000

    Update Machine_Learning_Function_Deployment.py

--- Machine_Learning_Function_Deployment.py
+++ Machine_Learning_Function_Deployment.py
@@ -64,7 +64,7 @@ if delete=="Yes":
     col=st.multiselect("Please select the columns that you want to delete",df.columns)
     done=st.button("Do you want to delete the colums")
     if done==True:
-        col1=col
+        col1=list(set(col))
         df.drop(columns=col1,inplace=True)
     st.dataframe(df)
 #Checking and filling the missing values in the data
